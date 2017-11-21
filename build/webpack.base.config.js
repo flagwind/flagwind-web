2c6178f66e037a84a6f997765b7bdc26fb1eb36e
jason <jasonsoop@gmail.com>
2017-11-21 11:15:10 +0800

    添加核心依赖。

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -24,12 +24,12 @@ module.exports =
             {
                 test: /\.ts$/,
                 exclude: /node_modules/,
-                enforce: 'pre',
-                loader: 'tslint-loader'
+                enforce: "pre",
+                loader: "tslint-loader"
             },
             {
                 test: /\.tsx?$/,
-                loader: 'ts-loader',
+                loader: "ts-loader",
                 exclude: /node_modules/,
                 options: {
                     appendTsSuffixTo: [/\.vue$/],
