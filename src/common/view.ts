f87b7bfd3ff4a6bb33478754332b5aa877d1e1a3
Jason <jasonsoop@gmail.com>
2017-12-15 22:30:18 +0800

    为 view 添加一个默认服务实例。

--- src/common/view.ts
+++ src/common/view.ts
@@ -8,6 +8,7 @@
  * Copyright (C) 2010-2017 Flagwind Inc. All rights reserved. 
  */
 
+import flagwind from "flagwind-core";
 import { Component } from "./component";
 
 /**
@@ -17,5 +18,14 @@ import { Component } from "./component";
  */
 export class View extends Component
 {
-    
+    /**
+     * 获取默认服务容器实例。
+     * @protected
+     * @property
+     * @returns flagwind.IServiceProvider
+     */
+    protected get serviceProvier(): flagwind.IServiceProvider
+    {
+        return flagwind.ServiceProviderFactory.instance.default;
+    }
 }
