02afd7d67921a564d36a6f98537c3cad63989a00
Jason <jasonsoop@gmail.com>
2017-12-17 23:56:47 +0800

    完成数字计数组件。

--- examples/views/components/number-counter.vue
+++ examples/views/components/number-counter.vue
@@ -1,21 +1,56 @@
 <template>
     <l-article>
         <h1>NumberCounter 数字计数</h1>
-        <p>基础组件，触发业务逻辑时使用。</p>
+        <h2>概述</h2>
+        <p>根据数值的增减播放相应的过渡动画</p>
         <h2>代码示例</h2>
-        
         <u-example title="基本使用">
             <template slot="case">
-                <fw-number-counter></fw-number-counter>
+                <fw-number-counter :value="value1" style="font-size:46px;"></fw-number-counter>
+                <br />
+                <i-button @click="value1 += 10">加值</i-button>
+                <i-button @click="value1 -= 10">减值</i-button>
             </template>
             <template slot="desc">
+                <p>基本用法，通过设置<code>value</code>的增减实现数据的上下翻动。</p>
             </template>
-            <u-code slot="code" lang="html"></u-code>
+            <u-code slot="code" lang="html">{{code.generic}}</u-code>
+        </u-example>
+
+        <u-example title="格式化">
+            <template slot="case">
+                <fw-number-counter :value="value2" format="(,ddd).dd" style="font-size:46px;"></fw-number-counter>
+                <br />
+                <i-button type="info" @click="value2 += 100">加值</i-button>
+                <i-button type="warning" @click="value2 -= 100">减值</i-button>
+            </template>
+            <template slot="desc">
+                <p>格式化数值，通过设置<code>format</code>属性，可自定义数值的格式化显示。</p>
+                <p>例: (,ddd) > 12,345,678</p>
+                <p>例: (,ddd).dd > 12,345,678.09</p>
+                <p>例: (.ddd),dd > 12.345.678,09</p>
+                <p>例: ( ddd),dd > 12 345 678,09</p>
+                <p>例:  d > 12345678</p>
+            </template>
+            <u-code slot="code" lang="html">{{code.format}}</u-code>
+        </u-example>
+
+        <u-example title="动画效果">
+            <template slot="case">
+                <fw-number-counter :value="value3" animation="count" style="font-size:46px;"></fw-number-counter>
+                <br />
+                <i-button type="success" @click="value3 = 123456">加值</i-button>
+                <i-button type="error" @click="value3 = 0">减值</i-button>
+            </template>
+            <template slot="desc">
+                <p>通过设置<code>animation</code>属性为<code>slide</code>、<code>count</code>使用不同的数值变动效果。</p>
+            </template>
+            <u-code slot="code" lang="html">{{code.animation}}</u-code>
         </u-example>
 
         <div class="api">
             <h2>API</h2>
-            <h3>Button props</h3>
+            <h3>属性</h3>
             <table>
                 <thead>
                     <tr>
@@ -27,52 +62,51 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <td>type</td>
-                        <td>按钮类型，可选值为<code>primary</code>、<code>ghost</code>、<code>dashed</code>、<code>text</code>、<code>info</code>、<code>success</code>、<code>warning</code>、<code>error</code>或者不设置</td>
+                        <td>value</td>
+                        <td>绑定的数值</td>
+                        <td>Number</td>
+                        <td>0</td>
+                    </tr>
+                    <tr>
+                        <td>format</td>
+                        <td>数值格式化字符串模板</td>
                         <td>String</td>
+                        <td>(d).dd</td>
+                    </tr>
+                    <tr>
+                        <td>formatFunction</td>
+                        <td>自定义数值格式化函数</td>
+                        <td>Function</td>
                         <td>-</td>
                     </tr>
                     <tr>
-                        <td>size</td>
-                        <td>按钮大小，可选值为<code>large</code>、<code>small</code>、<code>default</code>或者不设置</td>
+                        <td>animation</td>
+                        <td>动画类型，可选参数为<code>slide</code>、<code>count</code></td>
                         <td>String</td>
-                        <td>-</td>
+                        <td>slide</td>
                     </tr>
                     <tr>
-                        <td>shape</td>
-                        <td>按钮形状，可选值为<code>circle</code>或者不设置</td>
-                        <td>String</td>
-                        <td>-</td>
+                        <td>duration</td>
+                        <td>动画持续时间</td>
+                        <td>Number</td>
+                        <td>2000</td>
                     </tr>
+                </tbody>
+            </table>
+            <h3>事件</h3>
+            <table>
+                <thead>
                     <tr>
-                        <td>long</td>
-                        <td>开启后，按钮的长度为 100%</td>
-                        <td>Boolean</td>
-                        <td>false</td>
+                        <th>事件名</th>
+                        <th>说明</th>
+                        <th>返回值</th>
                     </tr>
+                </thead>
+                <tbody>
                     <tr>
-                        <td>html-type</td>
-                        <td>设置<code>button</code>原生的<code>type</code>，可选值为<code>button</code>、<code>submit</code>、<code>reset</code></td>
-                        <td>String</td>
-                        <td>button</td>
-                    </tr>
-                    <tr>
-                        <td>disabled</td>
-                        <td>设置按钮为禁用状态</td>
-                        <td>Boolean</td>
-                        <td>false</td>
-                    </tr>
-                    <tr>
-                        <td>loading</td>
-                        <td>设置按钮为加载中状态</td>
-                        <td>Boolean</td>
-                        <td>false</td>
-                    </tr>
-                    <tr>
-                        <td>icon</td>
-                        <td>设置按钮的图标类型</td>
-                        <td>String</td>
-                        <td>-</td>
+                        <td>on-complete</td>
+                        <td>动画播放完毕时触发</td>
+                        <td>无</td>
                     </tr>
                 </tbody>
             </table>
@@ -82,10 +116,10 @@
 
 <script lang="ts">
 import { component, View } from "src/index";
-import * as code from "examples/codes/button";
+import * as code from "examples/codes/number-counter";
 
 @component
-export default class Button extends View
+export default class NumberCounter extends View
 {
     /**
      * 演示需要的代码。
@@ -94,5 +128,29 @@ export default class Button extends View
      * @returns any
      */
     protected code: any = code;
+
+    /**
+     * 示例一使用的数值。
+     * @protected
+     * @member
+     * @returns number
+     */
+    protected value1: number = 5210;
+
+    /**
+     * 示例二使用的数值。
+     * @protected
+     * @member
+     * @returns number
+     */
+    protected value2: number = 12345.67;
+
+    /**
+     * 示例三使用的数值。
+     * @protected
+     * @member
+     * @returns number
+     */
+    protected value3: number = 1000;
 }
 </script>
